e48027b88011a4e6c90bb5481e57f9225e087475
odenra <ilkerkaraoden@gmail.com>
2024-03-22 19:13:39 +0300

    react header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./header/Header";
+
+function App() {
+  
+  return (
+  <>
+    <Header />
+  </>
+  );
+}
+
+export default App;
---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='title'>Kurslar</div>
+    </div>
+  )
+}
+
+export default Header
